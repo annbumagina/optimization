096b5737a87a304e05b3087eb25a122cb50ee24e
fedleonid <fedleonid@yandex-team.ru>
2021-04-09 21:01:41 +0300

    add conjugate gradient method

---
+++ lab2/conjugate_gradient.py
@@ -0,0 +1,50 @@
+from typing import Callable
+import numpy as np
+import numdifftools as nd
+
+from lab1.history.history import GradientHistory
+
+
+class FletcherReeves:
+    def __init__(self,
+                 target: Callable,
+                 start_point: np.ndarray,
+                 optimize_method: Callable,
+                 eps: float):
+        self.target = target
+        self.start_point = start_point
+        self.optimize_method = optimize_method
+        self.eps = eps
+        self.it = 0
+        self.result = None
+        self.history = GradientHistory(['x', 'alpha'])
+
+    def compute(self):
+        fgrad = nd.Gradient(self.target)
+        x = self.start_point
+        fgrad_x = fgrad(x)
+        vector = fgrad_x
+
+        self.history.add_iteration(x, '-')
+
+        while True:
+            def for_optimize(t):
+                return self.target(x - t * vector)
+
+            alpha = self.optimize_method(for_optimize, 0, 1000, self.eps, lambda f1, f2: f1 < f2)
+            xnew = x - alpha * fgrad(x)
+
+            self.history.add_iteration(xnew, alpha)
+
+            if np.linalg.norm(xnew - x) < self.eps:
+                self.result = xnew
+                break
+
+            fgrad_xnew = fgrad(xnew)
+            gamma = np.linalg.norm(fgrad_xnew) ** 2 / np.linalg.norm(fgrad_x) ** 2
+            vector = fgrad_xnew + gamma * vector
+            fgrad_x = fgrad_xnew
+            x = xnew
+
+    def get_points(self):
+        return self.history.get_column(1)
---
+++ lab2/test.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+from lab1.methods.golden_section import GoldenSectionMethod
+from lab1.methods.wrapper import wrap_method
+from lab2.conjugate_gradient import FletcherReeves
+
+
+def conjugate_gradient_test():
+    """
+    Tests gradient on simple functions and prints statistics
+    """
+    print("========================")
+    print("   CONJUGATE GRADIENT   ")
+    print("========================")
+    print()
+
+    f = ["lambda x: 10 * x[0] * x[0] + x[1] * x[1] + 2 * x[1]"]
+    start = [np.array([10, 10.]), np.array([10, -10.])]
+    extremum = [np.array([0, -1.]), np.array([0, 0.])]
+    eps = 0.001
+    for i in range(len(f)):
+        print("function of interest:", f[i])
+        compute_result = wrap_method(GoldenSectionMethod)
+        gradient = FletcherReeves(eval(f[i]), start[i], compute_result, eps)
+        gradient.compute()
+        gradient.history.print_history(GoldenSectionMethod.name(), extremum[i], gradient.result, eps, f[i])
+        assert np.linalg.norm(gradient.result - extremum[i]) < 2 * eps
+        print("\tOk\n")
+        print()
+
+
+conjugate_gradient_test()
