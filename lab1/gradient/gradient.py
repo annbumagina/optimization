fix gradient and start points

lab1/gradient/gradient.py
@@ -30,15 +30,17 @@ class Gradient:
         self.history.add_iteration(x, fgrad(x), '-')
 
         while True:
+            grad_x = fgrad(x)
+
             def for_optimize(t):
                 self.history.op(x.size * 2)
-                return self.target(x - t * fgrad(x))
+                return self.target(x - t * grad_x)
 
-            alpha = self.optimize_method(for_optimize, 0, 1000, self.eps, lambda f1, f2: f1 < f2)
-            xnew = x - alpha * fgrad(x)
+            alpha = self.optimize_method(for_optimize, 0, 1., self.eps, lambda f1, f2: f1 < f2)
+            xnew = x - alpha * grad_x
 
             self.history.op(x.size * 2)
-            self.history.add_iteration(xnew, fgrad(x), alpha)
+            self.history.add_iteration(xnew, grad_x, alpha)
 
             if np.linalg.norm(xnew - x) < self.eps:
                 self.result = xnew

lab2/task2.py
@@ -14,7 +14,7 @@ from lab1.gradient.gradient import Gradient
     Если вайт лист не пустой, то будут считаться функции только из этого листа
 """
 
-white_list = [1]
+white_list = [2]
 
 box = {
     1: {
@@ -26,31 +26,31 @@ box = {
     2: {
         'function': 'lambda x: 100 * (x[1] - x[0]**2)**2 + (1 - x[0])**2',
         'extremum': np.array([1, 1]),
-        'starts': [np.array([1, 1]), np.array([2, 2]), np.array([10, 10]), np.array([0, 0]), np.array([-3, 3]),
-                   np.array([1, 3]), np.array([0.5, 1])],
+        'starts': [np.array([1, 1]), np.array([2, 2]), np.array([0, 0]), np.array([-3, 3]),
+                   np.array([2, 4]), np.array([1, 0.5])],
     },
     3: {
-        'function': 'lambda x: 2 * math.exp((-(x[0] - 1) / 2)**2 - ((x[1] - 1) / 1)**2) '
-                    '+ 3 * math.exp((-(x[0] - 2) / 3)**2 - ((x[1] - 3) / 2)**2)',
-        'extremum': np.array([0, 0]),
-        'starts': [np.array([0, 0]), np.array([-5, 5])],
+        'function': 'lambda x: -(2 * math.exp(-((x[0] - 1) / 2)**2 - ((x[1] - 1) / 1)**2) '
+                    '+ 3 * math.exp(-((x[0] - 2) / 3)**2 - ((x[1] - 3) / 2)**2))',
+        'extremum': np.array([1.2630, 1.33439]),
+        'starts': [np.array([0, 0]), np.array([3, 0])],
     }
 }
 
-methods = [GoldenSectionMethod]
+methods = [Gradient, FletcherReeves, Newton]
 # epsilon = 0.0001 fix
 epsilon = 0.00001
 
 
-@profile
-def _test(function: str, start_pos, extremum, opt_method, eps):
+#@profile
+def _test(method, function: str, start_pos, extremum, eps):
     print("Testing function:", function)
-    compute_result = wrap_method(opt_method)
-    gradient = Gradient(eval(function), start_pos, compute_result, eps)
+    compute_result = wrap_method(GoldenSectionMethod)
+    gradient = method(eval(function), start_pos, compute_result, eps)
     gradient.compute()
-    # gradient.history.print_history(opt_method.name(), extremum, gradient.result, eps, function)
+    #gradient.history.print_history(method.name(), extremum, gradient.result, eps, function)
 
-    if np.linalg.norm(gradient.result - extremum) < eps:
+    if np.linalg.norm(gradient.result - extremum) < 0.1:
         print("\tTest completed!\n")
     else:
         print("\n\tTest failed! " + "expected: " + str(extremum) + " result: " + str(gradient.result) + " \n")
@@ -75,7 +75,7 @@ if __name__ == '__main__':
                 try:
                     print("Start position: " + str(start))
                     p = multiprocessing.Process(
-                        target=_test(box[i]["function"], start, box[i]["extremum"], method, epsilon))
+                        target=_test(method, box[i]["function"], start, box[i]["extremum"], epsilon))
                     p.start()
 
                     p.join(2)  # 2 seconds