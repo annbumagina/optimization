cafdfd3757909f95b8d5894e46e2a5674f46ade9
annbumagina <annbumagina@ya.ru>
2021-04-29 17:11:32 +0300

    identity matrix

--- lab3/simplex.py
+++ lab3/simplex.py
@@ -25,28 +25,39 @@ class SimplexMethod:
         m = len(self.A)
         n = len(self.A[0])
 
-        t = 0
+        rows = np.zeros(m, dtype=bool)
         for i in range(n):
             if self.basis[i] == .0:
                 continue
 
+            t = -1
+            for j in range(m):
+                if self.A[j][i] != 0 and not rows[j]:
+                    t = j
+                    rows[j] = True
+                    break
+            if t == -1:
+                raise Exception('wrong basis')
+
             self.s_x[t] = i
             self.s_v[t] = self.basis[i]
 
+            self.b[t] = self.b[t] / self.A[t][i]
             self.A[t] = self.A[t] / self.A[t][i]
             for p in range(m):
                 if p == t:
                     continue
                 k = self.A[p][i] / self.A[t][i]
                 self.A[p] = self.A[p] - self.A[t] * k
-
-            t += 1
+                self.b[p] = self.b[p] - self.b[t] * k
 
         for i in range(n):
             summ = 0.0
             for j in range(m):
                 summ += self.A[j][i] * self.c[self.s_x[j]]
             self.f[i] = summ - self.c[i]
+
+        self.s_v = self.b
 
     # получить текущее решение
     def get_solution(self):
@@ -73,9 +84,10 @@ class SimplexMethod:
 
         def compute_q(i):
             with np.errstate(divide='ignore'):
-                return abs(np.float64(self.s_v[i]) / np.float64(self.A[i][column_idx]))
+                return np.float64(self.s_v[i]) / np.float64(self.A[i][column_idx])
 
         Q = np.array([compute_q(i) for i in range(m)])
+        Q[Q < 0] = np.inf
         row_idx = np.argmin(Q)
 
         self.s_v[row_idx] = self.s_v[row_idx] / self.A[row_idx][column_idx]
